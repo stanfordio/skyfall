1f742190784ad6c130a33c7c1bfbdfe4c6d4227c
R. Miles McCain <github@sendmiles.email>
2023-11-12 21:58:08 -0800

    Add prelim stream command and ws-url flag (no compilation yet)

--- cmd/main.go
+++ cmd/main.go
@@ -18,18 +18,32 @@ func run(args []string) {
 		Version: "prerelease",
 		Commands: []*cli.Command{
 			{
-				Name:    "test",
+				Name:    "stream",
 				Aliases: []string{"t"},
-				Usage:   "test command",
-				Action: func(cCtx *cli.Context) error {
-					fmt.Println("test")
-					return nil
-				},
+				Usage:   "Sip from the firehose",
+				Action:  stream,
 			},
 		},
 	}
 
+    app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			Name:  "ws-url",
+			Usage: "full websocket path to the ATProto SubscribeRepos XRPC endpoint",
+			Value: "wss://bsky.social/xrpc/com.atproto.sync.subscribeRepos",
+		},
+    }
+
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func stream(cctx *cli.Context) error {
+    u, err := url.Parse(cctx.String("ws-url"))
+	if err != nil {
+		log.Fatalf("failed to parse ws-url: %+v", err)
+	}
+
+
 }
