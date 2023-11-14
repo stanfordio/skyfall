ecff2b008d8167437056afb6749e69d20b8364f6
R. Miles McCain <github@sendmiles.email>
2023-11-13 21:21:59 -0800

    Add stream command with worker scaling
    This commit adds a new `stream` command to the CLI that allows users to
    sip from the firehose. The command includes a `worker-count` flag to
    specify the number of workers to scale to. This commit also includes
    updates to the `Stream` struct and `BeginStreaming` function to handle
    the new command and worker scaling.

--- pkg/stream.go
+++ pkg/stream.go
@@ -6,29 +6,68 @@ import (
 	"bytes"
 	"context"
 	"fmt"
-	"os"
+	"net/http"
+	"net/url"
 	"strings"
-	"sync"
-	"time"
-	"log"
 
-	"github.com/araddon/dateparse"
 	comatproto "github.com/bluesky-social/indigo/api/atproto"
-
-	"github.com/bluesky-social/indigo/events"
 	"github.com/bluesky-social/indigo/repo"
 	"github.com/bluesky-social/indigo/repomgr"
-	"github.com/bluesky-social/indigo/api/bsky"
+	log "github.com/sirupsen/logrus"
+
+	// "github.com/bluesky-social/indigo/api/bsky"
+	"time"
+
+	"github.com/bluesky-social/indigo/events"
+	"github.com/bluesky-social/indigo/events/schedulers/autoscaling"
+	lexutil "github.com/bluesky-social/indigo/lex/util"
+	"github.com/gorilla/websocket"
 )
 
 type Stream struct {
 	// SocketURL is the full websocket path to the ATProto SubscribeRepos XRPC endpoint
-	SocketURL string
+	SocketURL *url.URL
+	Events    chan string // TODO: structured data
+}
+
+func (s *Stream) BeginStreaming(ctx context.Context, workerCount int) error {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	scalingSettings := autoscaling.DefaultAutoscaleSettings()
+	scalingSettings.MaxConcurrency = workerCount
+	scalingSettings.AutoscaleFrequency = time.Second
+
+	pool := autoscaling.NewScheduler(scalingSettings, s.SocketURL.Host, s.HandleStreamEvent)
+
+	log.Infof("connecting to WebSocket at: %s", s.SocketURL.String())
+	c, _, err := websocket.DefaultDialer.Dial(s.SocketURL.String(), http.Header{
+		"User-Agent": []string{"sonar/1.0"},
+	})
+
+	if err != nil {
+		log.Infof("failed to connect to websocket: %v", err)
+		return err
+	}
+	defer c.Close()
+
+	go func() {
+		err = events.HandleRepoStream(ctx, c, pool)
+		log.Infof("HandleRepoStream returned unexpectedly: %+v...", err)
+		cancel()
+	}()
+
+	select {
+	case <-ctx.Done():
+		log.Infof("shutting down...")
+	}
+
+	return nil
 }
 
 func (s *Stream) HandleStreamEvent(ctx context.Context, xe *events.XRPCStreamEvent) error {
 	if xe.Error != nil {
-		// TODO: Log the error
+		log.Errorf("error handling stream event: %+v", xe.Error)
 	}
 
 	if xe.RepoCommit != nil {
@@ -39,33 +78,27 @@ func (s *Stream) HandleStreamEvent(ctx context.Context, xe *events.XRPCStreamEve
 
 func (s *Stream) HandleRepoCommit(ctx context.Context, evt *comatproto.SyncSubscribeRepos_Commit) error {
 	// TODO: Actually do something with the event. Below is copied from the Sonar command.
-	
-	processedAt := time.Now()
 
 	rr, err := repo.ReadRepoFromCar(ctx, bytes.NewReader(evt.Blocks))
 	if err != nil {
-		log.Errorf("failed to read repo from car: %+v", err)
+		log.Warnf("failed to read repo from car: %+v", err)
 		return nil
-	}
-
-	if evt.Rebase {
-		log.Debug("rebase")
 	}
 
 	// Parse time from the event time string
 	evtCreatedAt, err := time.Parse(time.RFC3339, evt.Time)
 	if err != nil {
-		log.Errorf("error parsing time: %+v", err)
+		log.Warnf("error parsing time: %+v", err)
 		return nil
+	} else {
+		log.Infof("event created at: %+v", evtCreatedAt)
 	}
-	
+
 	for _, op := range evt.Ops {
 		collection := strings.Split(op.Path, "/")[0]
 
 		ek := repomgr.EventKind(op.Action)
-		log = log.With("action", op.Action, "collection", collection)
-
-		opsProcessedCounter.WithLabelValues(op.Action, collection, s.SocketURL).Inc()
+		log_wf := log.WithFields(log.Fields{"action": op.Action, "collection": collection})
 
 		switch ek {
 		case repomgr.EvtKindCreateRecord, repomgr.EvtKindUpdateRecord:
@@ -73,34 +106,24 @@ func (s *Stream) HandleRepoCommit(ctx context.Context, evt *comatproto.SyncSubsc
 			rc, rec, err := rr.GetRecord(ctx, op.Path)
 			if err != nil {
 				e := fmt.Errorf("getting record %s (%s) within seq %d for %s: %w", op.Path, *op.Cid, evt.Seq, evt.Repo, err)
-				log.Errorf("failed to get a record from the event: %+v", e)
+				log_wf.Errorf("failed to get a record from the event: %+v", e)
 				break
 			}
 
 			// Verify that the record cid matches the cid in the event
 			if lexutil.LexLink(rc) != *op.Cid {
 				e := fmt.Errorf("mismatch in record and op cid: %s != %s", rc, *op.Cid)
-				log.Errorf("failed to LexLink the record in the event: %+v", e)
+				log_wf.Errorf("failed to LexLink the record in the event: %+v", e)
 				break
 			}
-
-			var recCreatedAt time.Time
-			var parseError error
 
 			// Unpack the record and process it
 			switch rec := rec.(type) {
 			default:
-				log.Warnf("unknown record type: %+v", rec)
+				log_wf.Warnf("unknown record type: %+v", rec)
 			}
-			if parseError != nil {
-				log.Errorf("error parsing time: %+v", parseError)
-				continue
-			}
-			if !recCreatedAt.IsZero() {
-				lastEvtCreatedAtGauge.WithLabelValues(s.SocketURL).Set(float64(recCreatedAt.UnixNano()))
-				lastEvtCreatedRecordCreatedGapGauge.WithLabelValues(s.SocketURL).Set(float64(evtCreatedAt.Sub(recCreatedAt).Seconds()))
-				lastRecordCreatedEvtProcessedGapGauge.WithLabelValues(s.SocketURL).Set(float64(processedAt.Sub(recCreatedAt).Seconds()))
-			}
+
+			s.Events <- fmt.Sprintf("event: %+v", rec)
 
 		case repomgr.EvtKindDeleteRecord:
 		default:
@@ -108,6 +131,5 @@ func (s *Stream) HandleRepoCommit(ctx context.Context, evt *comatproto.SyncSubsc
 		}
 	}
 
-	eventProcessingDurationHistogram.WithLabelValues(s.SocketURL).Observe(time.Since(processedAt).Seconds())
 	return nil
 }
