079411748fab0a80df09e286d8ea315ce9fe442c
R. Miles McCain <github@sendmiles.email>
2023-11-14 18:21:57 -0800

    Add support for looking up posts and profiles, and cache results for 24 hours.

--- pkg/hydrator/hydrator.go
+++ pkg/hydrator/hydrator.go
@@ -92,8 +92,45 @@ func (h *Hydrator) lookupIdentity(identifier string) (identity *atpidentity.Iden
 	return
 }
 
-func (h *Hydrator) lookupProfile(identity *atpidentity.Identity) (profile *bsky.ActorDefs_ProfileViewDetailed, err error) {
+func (h *Hydrator) lookupProfileFromIdentity(identity *atpidentity.Identity) (profile *bsky.ActorDefs_ProfileViewDetailed, err error) {
 	profile, err = bsky.ActorGetProfile(h.Context, h.Client, identity.Handle.String())
+	return
+}
+
+func (h *Hydrator) lookupProfile(did string) (profile *bsky.ActorDefs_ProfileViewDetailed, err error) {
+	identity, err := h.lookupIdentity(did)
+	if err != nil {
+		return
+	}
+	profile, err = h.lookupProfileFromIdentity(identity)
+	return
+}
+
+func (h *Hydrator) lookupPost(atUrl string) (post *bsky.FeedDefs_PostView, err error) {
+	// Check the cache first
+	cachedValue, found := h.Cache.Get(atUrl)
+
+	if found && cachedValue != nil {
+		post = cachedValue.(*bsky.FeedDefs_PostView)
+		return
+	}
+
+	log.Debugf("Cache miss for %s", atUrl)
+
+	output, err := bsky.FeedGetPosts(h.Context, h.Client, []string{atUrl})
+	if err != nil {
+		return
+	}
+
+	if len(output.Posts) == 0 {
+		err = fmt.Errorf("no posts found for %s", atUrl)
+		return
+	}
+
+	post = output.Posts[0]
+
+	h.Cache.SetWithTTL(atUrl, post, 1, time.Duration(1)*time.Hour*24)
+
 	return
 }
 
@@ -113,7 +150,7 @@ func (h *Hydrator) Hydrate(val interface{}, actorDid string) (result map[string]
 		identity = nil
 	}
 
-	profile, err := h.lookupProfile(identity)
+	profile, err := h.lookupProfileFromIdentity(identity)
 	if err != nil {
 		log.Warnf("Failed to get profile for actor: %s", actorDid)
 		profile = nil
@@ -123,6 +160,50 @@ func (h *Hydrator) Hydrate(val interface{}, actorDid string) (result map[string]
 	result["_ActorDid"] = actorDid
 	result["_ActorIdentity"] = identity
 	result["_ActorProfile"] = profile
+
+	// Add the pulled time to the map, iso8601
+	result["_Pulled"] = time.Now().Format(time.RFC3339)
+
+	// Depending on the type, add additional information
+	switch val := val.(type) {
+	// For types, it can be helpful to look at https://github.com/bluesky-social/indigo/blob/49a1572716a6cccde22022c4264b62acbab43bc2/sonar/sonar.go#L227
+	case *bsky.FeedLike:
+		// Lookup the actual post (basic author info will be included)
+		post, err := h.lookupPost(val.Subject.Uri)
+		if err != nil {
+			log.Warnf("Failed to get post for like: %s", val.Subject.Uri)
+			post = nil
+		}
+		result["_LikedPost"] = post
+	case *bsky.FeedRepost:
+		// Lookup the actual post (basic author info will be included)
+		post, err := h.lookupPost(val.Subject.Uri)
+		if err != nil {
+			log.Warnf("Failed to get post for repost: %s", val.Subject.Uri)
+			post = nil
+		}
+		result["_RepostedPost"] = post
+	case *bsky.GraphBlock:
+		// Lookup the blocked user
+		profile, err := h.lookupProfile(val.Subject)
+		if err != nil {
+			log.Warnf("Failed to get profile for blocked user: %s", val.Subject)
+			profile = nil
+		}
+		result["_BlockedProfile"] = profile
+	case *bsky.GraphFollow:
+		// Lookup the followed user
+		profile, err := h.lookupProfile(val.Subject)
+		if err != nil {
+			log.Warnf("Failed to get profile for followed user: %s", val.Subject)
+			profile = nil
+		}
+		result["_FollowedProfile"] = profile
+	case *bsky.ActorProfile:
+		// Nothing to do
+	case *bsky.FeedPost:
+		// Nothing to do
+	}
 
 	return
 }
