99c86002575c65228d21bcaead4aed556ba95a9e
R. Miles McCain <github@sendmiles.email>
2023-11-14 17:53:12 -0800

    Add authentication flags and functions for stream command

--- pkg/hydrator/hydrator.go
+++ pkg/hydrator/hydrator.go
@@ -7,6 +7,7 @@ import (
 
 	log "github.com/sirupsen/logrus"
 
+	"github.com/bluesky-social/indigo/api/bsky"
 	"github.com/bluesky-social/indigo/atproto/identity"
 	atpidentity "github.com/bluesky-social/indigo/atproto/identity"
 	"github.com/bluesky-social/indigo/atproto/syntax"
@@ -18,12 +19,13 @@ import (
 
 type Hydrator struct {
 	Cache             *ristretto.Cache
+	AuthInfo          *xrpc.AuthInfo
 	Context           context.Context
 	Client            *xrpc.Client
 	IdentityDirectory identity.Directory
 }
 
-func MakeHydrator(ctx context.Context, cacheSize int64, hostDomain string) (*Hydrator, error) {
+func MakeHydrator(ctx context.Context, cacheSize int64, hostDomain string, authInfo *xrpc.AuthInfo) (*Hydrator, error) {
 	cache, err := ristretto.NewCache(&ristretto.Config{
 		NumCounters: 1e8, // number of keys to track frequency of
 		MaxCost:     cacheSize,
@@ -39,15 +41,31 @@ func MakeHydrator(ctx context.Context, cacheSize int64, hostDomain string) (*Hyd
 		Context: ctx,
 		Client: &xrpc.Client{
 			Client: indigoutil.RobustHTTPClient(),
-			Host:   fmt.Sprintf("https://%s", hostDomain),
+			Host:   "https://bsky.social", // We generally want to use the bsky.social host for all requests, since they are doing the indexing
+			Auth:   authInfo,
 		},
-		IdentityDirectory: identity.DefaultDirectory(),
+		IdentityDirectory: atpidentity.DefaultDirectory(),
+		AuthInfo:          authInfo,
 	}
 
 	return &h, nil
 }
 
-func (h *Hydrator) lookupIdentity(identifier string) (identity *identity.Identity, err error) {
+func (h *Hydrator) makeXRPCClientForHost(baseUrl string) *xrpc.Client {
+	// TODO: Cache these
+
+	return &xrpc.Client{
+		Client: h.Client.Client,
+		Host:   baseUrl, // It's called Host, but it's really the base URL
+		Auth:   h.AuthInfo,
+	}
+}
+
+func (h *Hydrator) getXRPCClientForPDS(identity *atpidentity.Identity) *xrpc.Client {
+	return h.makeXRPCClientForHost(identity.PDSEndpoint())
+}
+
+func (h *Hydrator) lookupIdentity(identifier string) (identity *atpidentity.Identity, err error) {
 	// Check the cache first
 	cachedValue, found := h.Cache.Get(identifier)
 
@@ -74,6 +92,11 @@ func (h *Hydrator) lookupIdentity(identifier string) (identity *identity.Identit
 	return
 }
 
+func (h *Hydrator) lookupProfile(identity *atpidentity.Identity) (profile *bsky.ActorDefs_ProfileViewDetailed, err error) {
+	profile, err = bsky.ActorGetProfile(h.Context, h.Client, identity.Handle.String())
+	return
+}
+
 func (h *Hydrator) Hydrate(val interface{}, actorDid string) (result map[string]interface{}, err error) {
 	err = nil
 
@@ -90,9 +113,16 @@ func (h *Hydrator) Hydrate(val interface{}, actorDid string) (result map[string]
 		identity = nil
 	}
 
+	profile, err := h.lookupProfile(identity)
+	if err != nil {
+		log.Warnf("Failed to get profile for actor: %s", actorDid)
+		profile = nil
+	}
+
 	// Add the actorDid and profile to the map
 	result["_ActorDid"] = actorDid
 	result["_ActorIdentity"] = identity
+	result["_ActorProfile"] = profile
 
 	return
 }
